8e5d246006e8d36ad853e9bbc0511c705433175d
Aquibe <aquibe@Aquibes-MacBook-Air-2.local>
2025-10-13 14:56:44 +0530

    adacemic calendar

---
+++ academics/sidebar.js
@@ -0,0 +1,32 @@
+document.getElementById('sidebar').innerHTML=`
+        <div class="sidebar-item" id="sb1">
+            <a href="./index.html"><p class="sidebar-text">DEPARTMENTS</p></a>
+        </div>
+        <div class="sidebar-item" id="sb2">
+            <a href="./programmes.html"><p class="sidebar-text">PROGRAMMES OFFERED</p></a>
+        </div>
+        <div class="sidebar-item" id="sb3">
+            <a href="./academic-calendar.html"><p class="sidebar-text">ACADEMIC CALENDAR</p></a>
+        </div>
+        <div class="sidebar-item " id="sb4">
+            <a href="./learning-outcomes.html"><p class="sidebar-text">LEARNING OUTCOMES</p></a>
+        </div>
+        <div class="sidebar-item" id="sb5">
+            <a href="./other-courses.html"><p class="sidebar-text">ADD-ON / CERTIFICATE COURSES</p></a>
+        </div>
+        <div class="sidebar-item" id="sb6">
+            <a href="./faculty.html"><p class="sidebar-text">FACULTY</p></a>
+        </div>
+        <div class="sidebar-item " id="sb7">
+            <a href="./nptel.html"><p class="sidebar-text">SWAYAM-NPTEL</p></a>
+        </div>
+        <div class="sidebar-item " id="sb8">
+            <a href="./study-in-india.html"><p class="sidebar-text">STUDY IN INDIA</p></a>
+        </div>
+        <div class="sidebar-item " id="sb9">
+            <a href="./iec.html"><p class="sidebar-text">INTERNAL EXAMINATION COMMITTEE</p></a>
+        </div>
+        <div class="sidebar-item " id="sb10">
+            <a href="./capacity-building.html"><p class="sidebar-text">CAPACITY BUILDING / SKILL ENHANCEMENT</p></a>
+        </div>
+`
